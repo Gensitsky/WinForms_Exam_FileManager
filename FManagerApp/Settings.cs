4c5472428d9e5bd16969552fcda00a87feaa6f69
Vadim Gensitsky <vadim.gensitsky@outlook.com>
2018-03-25 19:05:26 +0300

    Create programm

---
+++ FManagerApp/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.Runtime.Serialization;
+
+namespace FileManager
+{
+    [DataContract(Name = "Settings")]
+    public class Settings
+    {
+        [DataMember(Name = "FontSize")]
+        public short FontSize
+        { get;  private set; }
+
+        [DataMember(Name = "FontColor")]
+        public Color FontColor
+        { get; private set; }
+
+        public Settings() { }
+
+        public Settings(short fontSize, Color fontColor)
+        {
+            FontSize = fontSize;
+            FontColor = fontColor;
+        }
+    }
+}
